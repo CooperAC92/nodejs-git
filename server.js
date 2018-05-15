044c9f93c3b5a2486afe642f35e0cd798431c55c
adam <adamac92@googlemail.com>
2018-05-15 16:09:36 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,64 @@
+//load express
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+//Express Middleware
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = (`${now}: ${req.method} ${req.url}`);
+
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if(err){
+			console.log('Unable to append file.');
+		}
+	});
+	next();
+});
+
+// app.use((req,res,next) => {
+// 	res.render('maintenance.hbs');
+// });
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+//HTTP Request -JSON Object
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		welcomeMessage: 'Welcome to my Website',
+	});
+});
+
+//HTTP Request - /About page - HTML
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About Page',
+	});
+});
+
+//HTTP Request - /Bad page - JSON Obj
+app.get('/bad', (req, res) => {
+	res.send({
+		err: 'Request failed.'
+	});
+});
+
+//port to listen to for local connection
+app.listen(3000, () =>{
+	console.log('Server running on port 3000');
+});
